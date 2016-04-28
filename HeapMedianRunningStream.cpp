8448c9f6d0299b792d0901d0da76bea87614786d
Rajeesh5 <Rajeesh5@users.noreply.github.com>
2016-04-28 18:15:24 +0530

    Update HeapMedianRunningStream.cpp

--- HeapMedianRunningStream.cpp
+++ HeapMedianRunningStream.cpp
@@ -21,10 +21,63 @@ int getParentIndex(int i){if(i<=0)return -1; else return (i-1)/2; }
 
 void heapInitialize(Heap *h, int t)
 {
-	h->capacity
-	arr=(int*)malloc(HEAP_LEN*sizeof(int));
+	h->capacity=HEAP_LEN;
+	h->arr=(int*)malloc(HEAP_LEN*sizeof(int));
+	h->type=t;
+	h->heapSize=0;	
+}
+
+void heapify(heap **h,int i)
+{
+	int lIndex=getLeftChildIndex(i);
+	int rIndex=getRightChildIndex(i);
+	int maxIndex=-1;
+	if(lIndex!=-1 && *h->arr[i] < *h->arr[lIndex])
+		maxIndex=lIndex;
+	else maxIndex=i;
+	if(rIndex!=-1 && *h->arr[maxIndex] < *h->arr[rIndex])
+		maxIndex=rIndex;
+	if(maxIndex!=i)
+	{
+		int tmp=*h->arr[maxIndex];
+		*h->arr[maxIndex]=*h->arr[i];
+		*h->arr[i]=tmp;
+		heapify(&h,maxIndex);
+	}
+}
+
+
+void printHeapArr(heap *h)
+{
+	for(int i=0;i< h->heapSize;i++)
+		printf("\d ", h->arr[i]);
+	printf("\n");
 	
 }
+
+int findTopOfHeap(heap *h)
+
+
+void insertEle(heap **minH,heap **maxH,int data)
+{
+	//Checking Balance Factor
+
+	if(*maxH->heapSize==0)
+	{
+		
+		
+	}
+	
+	else{
+		
+		
+		
+	}
+
+
+	
+}
+
 
 
 
@@ -32,6 +85,10 @@ void heapInitialize(Heap *h, int t)
 int main()
 {
 	int streamArr[]={1,9,0,1,7,5,12,9,3,9,2,20,22,15,19,3,3,8};
+	int streamLen=18,i=0;
+	for(i=0;i<streamLen;i++)
+		insertEle(minh,maxh,streamArr[i]);
+	
 	
 	
 	
@@ -39,6 +96,3 @@ int main()
 	
 	
 }
-
-
-
