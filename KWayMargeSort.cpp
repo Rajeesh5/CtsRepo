e4327458ce5ccd9590fa5f62af1b867731deb06c
Rajeesh5 <Rajeesh5@users.noreply.github.com>
2016-05-08 21:05:26 +0530

    Added files via upload

---
+++ KWayMargeSort.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+#define ELE_CNT 1000000
+#define MAX_ARR_SIZE 1000
+
+using namespace std;
+
+int main()
+{
+    FILE *fp=fopen("RandomFile.txt","r");
+    int *arr,i;
+
+
+
+
+    fclose(fp);
+    return 0;
+}
