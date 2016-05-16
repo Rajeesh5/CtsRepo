8367582134914e04210535043c34e942d2b6ebd5
Rajeesh5 <Rajeesh5@users.noreply.github.com>
2016-05-16 08:35:32 +0530

    Update MargeKSortedArray.cpp

--- MargeKSortedArray.cpp
+++ MargeKSortedArray.cpp
@@ -15,66 +15,9 @@ struct MinHeapNode
     int listIndex;
 };
 
-/*
-
-
-
-
-
-
-
-
-MinHeap::MinHeap(MinHeapNode a[], int size)
-{
-    heap_size = size;
-    harr = a;  // store address of array
-    int i = (heap_size - 1)/2;
-    while (i >= 0)
-    {
-        MinHeapify(i);
-        i--;
-    }
-}
-
-void MinHeap::MinHeapify(int i)
-{
-    int l = left(i);
-    int r = right(i);
-    int smallest = i;
-    if (l < heap_size && harr[l].element < harr[i].element)
-        smallest = l;
-    if (r < heap_size && harr[r].element < harr[smallest].element)
-        smallest = r;
-    if (smallest != i)
-    {
-        swap(&harr[i], &harr[smallest]);
-        MinHeapify(smallest);
-    }
-}
-
-void swap(MinHeapNode *x, MinHeapNode *y)
-{
-    MinHeapNode temp = *x;  *x = *y;  *y = temp;
-}
-
-
-
-
-
-*/
-
-void printArray(int arr[], int size)
-{
-   for (int i=0; i < size; i++)
-   {
-       cout << arr[i] << " ";
-	   //if((i+1)%n==0)cout<<"\n";
-   }
-}
-
-
 class MinHeap
 {
+public:
     MinHeapNode *harr;
     int heapSize;
 	int capacity;
@@ -82,14 +25,52 @@ public:
 
     MinHeap();
     void minHeapify(int );
-	void insertIntoHeap(int);
+	void insertIntoHeap(int,int,int);
 	void printHeap();
     int getLeftIndex(int i)  { if(2*i + 1 > heapSize) return -1; else return (2*i + 1); }
     int getRightIndex(int i) { if(2*i + 2 > heapSize) return -1; else return (2*i + 2); }
 	int getParentIndex(int i){ if(i<=0 || i >= heapSize)return -1; else return (i-1)/2; }
-    MinHeapNode getMin() { return harr[0]; }
-    //void replaceMin(MinHeapNode x) { harr[0] = x;  MinHeapify(0); }
+    MinHeapNode  extractMin();
+    friend void margeKArrays(int **,int ,int*);
 };
+
+void printArray(int arr[], int size)
+{
+   for (int i=0; i < size; i++)
+   {
+       cout << arr[i] << " ";
+	   if((i+1)%n==0)cout<<"\n";
+   }
+}
+
+MinHeapNode MinHeap::extractMin()
+{
+    MinHeapNode tmpMin = harr[0];
+    harr[0]=harr[heapSize-1];
+    heapSize--;
+    minHeapify(0);
+    return tmpMin;
+}
+
+void MinHeap::minHeapify(int index)
+{
+    int minIndex=index;
+    int leftIndex=getLeftIndex(index);
+    int rightIndex=getRightIndex(index);
+    if(leftIndex != -1 && harr[index].element > harr[leftIndex].element)
+        minIndex=leftIndex;
+    else minIndex=index;
+
+    if(rightIndex != -1 && harr[minIndex].element > harr[rightIndex].element )
+        minIndex=rightIndex;
+
+    if(minIndex != index)
+    {
+        swap(harr[index],harr[minIndex]);
+        minHeapify(minIndex);
+     }
+
+}
 
 void MinHeap::printHeap()
 {
@@ -97,9 +78,15 @@ void MinHeap::printHeap()
 		cout<<harr[i].element<<" ";
 	cout<<endl;
 }
-void swap(MinHeapNode *x, MinHeapNode *y)
-{
 
+void swapNode(MinHeapNode *x, MinHeapNode *y)
+{
+    	MinHeapNode* tmpSwap= new MinHeapNode();
+    	tmpSwap=x;
+		x=y;
+		y=tmpSwap;
+
+		//delete tmpSwap;
 
 }
 
@@ -115,7 +102,7 @@ MinHeap::MinHeap()
 	heapSize=0;
 }
 
-void MinHeap::insertIntoHeap(int data)
+void MinHeap::insertIntoHeap(int data,int lstNo,int lstIndex)
 {
 	int p;
 	if(heapSize+1 >= MAX_CAPACITY)
@@ -123,71 +110,52 @@ void MinHeap::insertIntoHeap(int data)
 		cout<<"HEAP IS FULL , Cant Insert more element\n";
 		return ;
 	}
-	harr[heapSize++].element=data;
+	harr[heapSize].element=data;
+	harr[heapSize].listNo=lstNo;
+	harr[heapSize].listIndex=lstIndex;
+	heapSize++;
 	p=getParentIndex(heapSize-1);
 	if(p<0)return;
 	for(int i=heapSize-1;i >=0 && harr[p].element >= data ;i=((i-1)/2))
 	{
-		int tmp=harr[p].element;
-		harr[p].element=harr[i].element;
-		harr[i].element=tmp;
-
+		swapNode(&harr[i],&harr[p]);
 		p=getParentIndex(p);
 	}
-	
-}
-
-/*
-
-int * mergeKArrays(int **arr, int k)
-{
-	int i;
-	MinHeap *mHeap=new MinHeap();
-	for(i=0;i<k;i++)
-		insertIntoHeap(arr[1][i]);
-
-	while(mHeap->heapSize>0)
-	{
-
-	}
 
 }
-*/
 
-
-
-
-
-
-
-
-int main()
+void margeKArrays()
 {
-
-    int arr[][n] = {{ 1,3,3,7,9,10,13},
+	int i,count=0;
+	int arr[][n] = {{ 1,3,3,7,9,10,13},
 					{2,3,4,7,7,9,10},
 					{5,6,7,9,10,11,14},
 					{9,11,13,14,16,20,22}};
 
 
     int k = sizeof(arr)/sizeof(arr[0]);
- 	printArray(arr[0],n*k);
-
+ 	int *output=new int[n*k];
 	MinHeap *mHeap=new MinHeap();
 
-	for (int i=0; i < k; i++)
-	{
-		for(int j=0; j < n ; j++)
-		mHeap->insertIntoHeap(arr[i][j]);
-	}
+	for(i=0;i<k;i++)
+		mHeap->insertIntoHeap(arr[i][0],i,0);
 
-	cout<<endl;
-	mHeap->printHeap();
-	
-	
-    //int *output = mergeKArrays(arr, k);
-    //cout << "Merged array is " << endl;
-    //printArray(output, n*k);
+	while( mHeap->heapSize > 0)
+	{
+
+        MinHeapNode tmp=mHeap->extractMin();
+        output[count++]=tmp.element;
+        if(tmp.listIndex<k)
+            mHeap->insertIntoHeap(arr[tmp.listNo][tmp.listIndex+1],tmp.listNo,tmp.listIndex+1);
+	}
+	printArray(output,k*n);
+
+}
+
+int main()
+{
+
+	margeKArrays();
 
     return 0;
 }
